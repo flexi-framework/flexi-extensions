7b05fcfd85b772816aa0388550253fbc18752a30
Jakob Duerrwaechter <iagduerr@iagpc151.iag.uni-stuttgart.de>
2019-02-08 15:56:51 +0100

    remove requirement for nRuns to be nParallelRuns*nSequentialRuns; also some cleanup

--- src/flexibatch.f90
+++ src/flexibatch.f90
@@ -20,17 +20,29 @@ PROGRAM FlexiBatch
 ! MODULES
 USE MOD_Globals
 USE MOD_Flexi
-USE MOD_TimeDisc,          ONLY:TimeDisc
-USE MOD_IO_HDF5, ONLY: OpenDataFile,CloseDataFile,File_ID
-USE MOD_HDF5_Input, ONLY: ReadAttribute
+USE MOD_TimeDisc   ,ONLY: TimeDisc
+USE MOD_IO_HDF5    ,ONLY: OpenDataFile,CloseDataFile,File_ID
+USE MOD_HDF5_Input ,ONLY: ReadAttribute
 IMPLICIT NONE
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
-INTEGER      :: nArgsLoc,iArg
+INTEGER                        :: nArgsLoc,iArg,Color
 CHARACTER(LEN=255),ALLOCATABLE :: ArgsLoc(:)
 CHARACTER(LEN=255)             :: StochFile
 !==================================================================================================================================
-! Initialize
+
+! read command line arguments
+
+nArgsLoc = COMMAND_ARGUMENT_COUNT()
+IF(nArgsLoc.LT.2) CALL Abort(__STAMP__,'Usage: ./flexi flexi.h5 flexi.ini')
+CALL GET_COMMAND_ARGUMENT(1,StochFile)
+
+ALLOCATE(ArgsLoc(nArgsLoc-1))
+DO iArg=2,nArgsLoc
+  CALL GET_COMMAND_ARGUMENT(iArg,ArgsLoc(iArg-1))
+END DO 
+
+! init global MPI
 
 #if USE_MPI
 CALL MPI_INIT(iError)
@@ -38,10 +50,10 @@ CALL MPI_COMM_RANK(MPI_COMM_WORLD, myGlobalRank     , iError)
 CALL MPI_COMM_SIZE(MPI_COMM_WORLD, nGlobalProcessors, iError)
 
 MPI_COMM_ACTIVE=MPI_COMM_WORLD
-
-nArgsLoc = COMMAND_ARGUMENT_COUNT()
-IF(nArgsLoc.LT.2) CALL Abort(__STAMP__,'Usage: ./flexi flexi.h5 flexi.ini')
-CALL GET_COMMAND_ARGUMENT(1,StochFile)
+#else
+myGlobalRank=0
+nGlobalProcessors=1
+#endif
 
 ! open StochFile, Get Attributes nRuns, nParallelRuns
 CALL OpenDataFile(StochFile,create=.FALSE.,single=.FALSE.,readOnly=.TRUE.)
@@ -49,29 +61,30 @@ CALL ReadAttribute(File_ID,'nRuns',1,IntScalar=nRuns)
 CALL ReadAttribute(File_ID,'nParallelRuns',1,IntScalar=nParallelRuns)
 CALL CloseDataFile()
 
-ALLOCATE(ArgsLoc(nArgsLoc-1))
-DO iArg=2,nArgsLoc
-  CALL GET_COMMAND_ARGUMENT(iArg,ArgsLoc(iArg-1))
-END DO 
-
 
 IF(MOD(nGlobalProcessors,nProcsPerRun).NE.0) CALL Abort(__STAMP__,'nProcs has to be a multiple of nProcsPerRun')
-nProcsPerRun=nGlobalProcessors/nParallelRuns
+nProcsPerRun = nGlobalProcessors/nParallelRuns
 iParallelRun = myGlobalRank/nProcsPerRun+1
 
 CALL MPI_COMM_SPLIT(MPI_COMM_WORLD,iParallelRun,myGlobalRank,MPI_COMM_FLEXI,iError) 
 
-IF(MOD(nRuns,nParallelRuns).NE.0) CALL Abort(__STAMP__,'nRuns has to be a multiple of nParallelRuns')
-nSequentialRuns= nRuns/nParallelRuns
-#else
-nParallelRuns=1
-nSequentialRuns=nRuns
-#endif
+!We now allow nRuns not to fit perfectly.
+!IF(MOD(nRuns,nParallelRuns).NE.0) CALL Abort(__STAMP__,'nRuns has to be a multiple of nParallelRuns')
+nSequentialRuns = nRuns / nParallelRuns
+
+! run FLEXI in loop
 
 DO iSequentialRun=1,nSequentialRuns
 
   iRun=iSequentialRun+nParallelRuns*(iSequentialRun-1)
 
+  ! During las sequential runs, some parallel runs might idle. We therefore split MPI_COMM_WORLD.
+  IF(iSequentialRun.EQ.nSequentialRuns)THEN
+    Color=MERGE(0,MPI_UNDEFINED,iRun.LE.nRuns)
+    CALL MPI_COMM_SPLIT(MPI_COMM_WORLD,color,myGlobalRank,MPI_COMM_ACTIVE,iError) 
+  END IF 
+
+  ! Initialize
   CALL InitFlexi(nArgsLoc-1,ArgsLoc,mpi_comm_loc=MPI_COMM_FLEXI)
   ! Run Simulation
   CALL TimeDisc()
