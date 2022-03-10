ba655b25bce5eac4369f3d7267d11eb12446a247
Marius Kurz <marius.kurz@iag.uni-stuttgart.de>
2022-03-10 18:34:02 +0100

    Added computation of the invariants of the gradient tensor

--- src/relexi/smartredis.f90
+++ src/relexi/smartredis.f90
@@ -1,4 +1,5 @@
 #include "flexi.h"
+#include "eos.h"
 
 MODULE MOD_SmartRedis
 
@@ -200,6 +201,7 @@ USE MOD_Globals
 USE MOD_PreProc
 USE MOD_SmartRedis_Vars
 USE MOD_Mesh_Vars       ,ONLY: nElems,nGlobalElems
+USE MOD_Lifting_Vars,       ONLY: gradUx,gradUy,gradUz
 #if USE_FFTW
 USE MOD_FFT_Vars,           ONLY: kmax
 USE MOD_Testcase_Vars,      ONLY: E_k
@@ -216,6 +218,7 @@ LOGICAL,INTENT(IN)          :: LastTimeStep
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 CHARACTER(LEN=255)             :: Key
+REAL                           :: inv(5,0:PP_N,0:PP_N,0:PP_N,1:nElems)
 INTEGER                        :: lastTimeStepInt(1),Dims(5),Dims_Out(5)
 INTEGER,PARAMETER              :: interval = 10   ! polling interval in milliseconds
 INTEGER,PARAMETER              :: tries    = HUGE(1)   ! Infinite number of polling tries
@@ -225,8 +228,10 @@ Dims = SHAPE(U)
 Dims_Out(:) = Dims(:)
 Dims_Out(5) = nGlobalElems
 
+CALL ComputeInvariants(gradUx,gradUy,gradUz,inv)
 Key = TRIM(FlexiTag)//"U"
-CALL GatheredWriteSmartRedis(5, Dims, U, TRIM(Key), Shape_Out = Dims_Out)
+CALL GatheredWriteSmartRedis(5, Dims, inv, TRIM(Key), Shape_Out = Dims_Out)
+!CALL GatheredWriteSmartRedis(5, Dims, U, TRIM(Key), Shape_Out = Dims_Out)
 
 IF (MPIroot .AND. (.NOT. firstTimeStep)) THEN
 #if USE_FFTW
@@ -252,6 +257,71 @@ END IF
 
 END SUBROUTINE ExchangeDataSmartRedis
 
+!==================================================================================================================================
+!> Computes the 5 invariants of the velocity gradient tensor, according to
+!>   - 'A more general effective-viscosity hypothesis', Pope, J. Fluid Mech.,1975.
+!> NOTE: The first of the 6 invariants (trace(S)) of 'matrix' is trivially zero for incompressible flows, thus resulting in only
+!>       5 non-trivial invariants.
+!==================================================================================================================================
+SUBROUTINE ComputeInvariants(gradUx, gradUy, gradUz, Invariants)
+! MODULES
+USE MOD_Globals
+USE MOD_PreProc
+USE MOD_SmartRedis_Vars
+USE MOD_Mesh_Vars       ,ONLY: nElems,nGlobalElems
+#if USE_FFTW
+USE MOD_FFT_Vars,           ONLY: kmax
+USE MOD_Testcase_Vars,      ONLY: E_k
+#endif
+#if EDDYVISCOSITY
+USE MOD_EddyVisc_Vars,      ONLY: Cs
+#endif
+IMPLICIT NONE
+!----------------------------------------------------------------------------------------------------------------------------------
+! INPUT/OUTPUT VARIABLES
+REAL,INTENT(IN)             :: gradUx(PP_nVarLifting,0:PP_N,0:PP_N,0:PP_NZ,1:nElems)
+REAL,INTENT(IN)             :: gradUy(PP_nVarLifting,0:PP_N,0:PP_N,0:PP_NZ,1:nElems)
+REAL,INTENT(IN)             :: gradUz(PP_nVarLifting,0:PP_N,0:PP_N,0:PP_NZ,1:nElems)
+REAL,INTENT(INOUT)          :: Invariants(5,0:PP_N,0:PP_N,0:PP_NZ,1:nElems)
+!----------------------------------------------------------------------------------------------------------------------------------
+! LOCAL VARIABLES
+INTEGER                :: i,j,k,iElem
+REAL                   :: S(3,3),S2(3,3)
+REAL                   :: W(3,3),W2(3,3)
+REAL                   :: mat(3,3)
+!==================================================================================================================================
+DO iElem=1,nElems
+  DO k=0,PP_NZ;DO j=0,PP_N; DO i=0,PP_N
+    mat(:,1) = gradUx(VELV,i,j,k,iElem)
+    mat(:,2) = gradUy(VELV,i,j,k,iElem)
+    mat(:,3) = gradUz(VELV,i,j,k,iElem)
+
+    S = 0.5*(mat+TRANSPOSE(mat)) ! Symmetric part
+    W = 0.5*(mat-TRANSPOSE(mat)) ! Anti-Symmetric part
+
+    ! Trace S^2
+    S2 = MATMUL(S,S)
+    Invariants(1,i,j,k,iElem) =  S2(1,1) +  S2(2,2) +  S2(3,3)
+
+    ! Trace W^2
+    W2 = MATMUL(W,W)
+    Invariants(2,i,j,k,iElem) =  W2(1,1) +  W2(2,2) +  W2(3,3)
+
+    ! Trace S^3
+    mat = MATMUL(S2,S)
+    Invariants(3,i,j,k,iElem) = mat(1,1) + mat(2,2) + mat(3,3)
+
+    ! Trace W^2*S
+    mat = MATMUL(W2,S)
+    Invariants(4,i,j,k,iElem) = mat(1,1) + mat(2,2) + mat(3,3)
+
+    ! Trace W^2*S^2
+    mat = MATMUL(W2,S2)
+    Invariants(5,i,j,k,iElem) = mat(1,1) + mat(2,2) + mat(3,3)
+
+  END DO; END DO; END DO
+END DO
+END SUBROUTINE ComputeInvariants
 
 !==================================================================================================================================
 !> Finalize SmartRedis Client
